124e6c20d25093a86ed9b4256a9324b0de12bfc8
Mohd Shahzan <khanshezyy786@gmail.com>
2024-11-13 21:19:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,18 +39,55 @@ app.get('/profile', isLoggedIn, async (req, res) => {
   res.render("profile", { user })
 })
 
-app.get('/like/:id', isLoggedIn, async (req, res) => {
-  let post = await postModel.findOne({ _id: req.params.id }).populate("user")
-
-  if (post.likes.indexOf(req.user.userid) === -1) {
-    post.likes.push(req.user.userid)
-  } else {
-    post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+// Add this route to your server code
+app.get('/explore', isLoggedIn, async (req, res) => {
+  try {
+    // Fetch all posts and populate the user who created each post
+    let posts = await postModel.find().populate('user');
+    res.render('explore', { posts, currentUser: req.user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error loading posts");
   }
+});
 
-  await post.save()
-  res.redirect("/profile")
-})
+
+// app.get('/like/:id', isLoggedIn, async (req, res) => {
+//   let post = await postModel.findOne({ _id: req.params.id }).populate("user")
+
+//   if (post.likes.indexOf(req.user.userid) === -1) {
+//     post.likes.push(req.user.userid)
+//   } else {
+//     post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+//   }
+
+//   await post.save()
+//   res.redirect("/explore")
+// })
+
+// new liking feature
+app.get('/like/:id', isLoggedIn, async (req, res) => {
+  try {
+    let post = await postModel.findOne({ _id: req.params.id });
+    const userId = req.user.userid;
+
+    // Toggle the like status
+    if (post.likes.includes(userId)) {
+      post.likes = post.likes.filter(id => id.toString() !== userId);
+    } else {
+      post.likes.push(userId);
+    }
+
+    await post.save();
+    
+    // Respond with the updated likes count
+    res.json({ success: true, likes: post.likes.length });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, message: "Error toggling like" });
+  }
+});
+
 
 app.get('/edit/:id', isLoggedIn, async (req, res) => {
   let post = await postModel.findOne({ _id: req.params.id })
@@ -60,20 +97,6 @@ app.get('/edit/:id', isLoggedIn, async (req, res) => {
 app.post('/update/:id', isLoggedIn, async (req, res) => {
   let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content })
   res.redirect("/profile")
-})
-
-app.post('/post', isLoggedIn, async (req, res) => {
-  let user = await userModel.findOne({ email: req.user.email })
-  let { content } = req.body;
-
-  let post = await postModel.create({
-    user: user._id,
-    content
-  })
-
-  user.posts.push(post._id)
-  await user.save()
-  res.redirect('/profile')
 })
 
 // New route for deleting a post
@@ -98,6 +121,20 @@ app.post('/delete/:id', isLoggedIn, async (req, res) => {
     console.error(err)
     res.status(500).send("Error deleting post")
   }
+})
+
+app.post('/post', isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email })
+  let { content } = req.body;
+
+  let post = await postModel.create({
+    user: user._id,
+    content
+  })
+
+  user.posts.push(post._id)
+  await user.save()
+  res.redirect('/profile')
 })
 
 app.post('/register', async (req, res) => {
